698e8cef6f17d95e35c5186224779ee43a32baac
rishikeshmisal <rishikeshmisal@gmail.com>
2018-04-12 19:13:07 +0530

    jpa dependency added

---
+++ src/main/java/game/config/Configuration.java
@@ -0,0 +1,11 @@
+package game.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by Rishikesh on 4/12/2018.
+ */
+@org.springframework.context.annotation.Configuration
+@EnableJpaRepositories
+public class Configuration {
+}
